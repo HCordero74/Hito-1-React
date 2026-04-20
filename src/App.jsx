7080f2b2c61fc2643313b0b48d065f4278ef2189
HCordero74 <hcordero74@yahoo.es>
2026-04-19 20:30:38 -0400

    Subir Hito-1

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/NavBar';
+import Home from './components/Home';
+import Footer from './components/Footer';
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
+  )
+}
+
+export default App
